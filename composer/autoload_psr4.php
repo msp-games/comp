d3696d1b338f8e3fa1238cc444c0a11c9c3d5390
Adam Roses Wight <awight@wikimedia.org>
2015-08-13 15:13:54 -0700

    Add monolog 1.12.0, keep in sync with DonationInterface monolog please
    Change-Id: I7ca99f796bc840fb9f2bdc0e9bb99067571708df

--- composer/autoload_psr4.php
+++ composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Wikimedia\\Composer\\' => array($vendorDir . '/wikimedia/composer-merge-plugin/src'),
+    'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
 );
